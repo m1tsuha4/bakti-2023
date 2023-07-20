285aaf1d04d14e03855f50755678d499748dd896
m1tsuha4 <114287774+m1tsuha4@users.noreply.github.com>
2023-07-20 09:57:19 +0700

    v.0

---
+++ database/migrations/2023_07_19_055703_create_profile_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('foto');
+            $table->string('nama_lengkap');
+            $table->string('fakultas');
+            $table->string('department');
+            $table->enum('jenis_kelamin',['L','P']);
+            $table->date('tanggal_lahir');
+            $table->integer('no_absen');
+            $table->integer('kelompok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile');
+    }
+};
